2a200fd039057baff11a80917c1630d7930e22e2
Hossam Mohsen <6911267+HossamMohsen7@users.noreply.github.com>
2024-10-05 05:46:19 +0300

    map api

--- src/routes/mapRouter.ts
+++ src/routes/mapRouter.ts
@@ -1,7 +1,25 @@
 import express from "express";
+import mapService from "../services/mapService.js";
 
 const router = express.Router();
 
-router.post("/data", async (req, res) => {});
+router.post("/data", async (req, res) => {
+  const { longitude, latitude, startDate, endDate, cloudCoverage } = req.body;
+
+  // Call the service
+
+  const metadata = await mapService.getMetadata(
+    longitude,
+    latitude,
+    startDate,
+    endDate,
+    cloudCoverage
+  );
+
+  res.status(200).json({
+    success: true,
+    data: metadata,
+  });
+});
 
 export default router;
---
+++ src/services/mapService.ts
@@ -0,0 +1,24 @@
+import got from "got";
+const getMetadata = async (
+  longitude: number,
+  latitude: number,
+  startDate: string,
+  endDate: string,
+  cloudCoverage: number = 100
+) => {
+  const response = await got("http://localhost:8069/search", {
+    json: {
+      longitude,
+      latitude,
+      start_date: startDate,
+      end_date: endDate,
+      cloud_cover: cloudCoverage,
+    },
+  }).json();
+
+  return response;
+};
+
+export default {
+  getMetadata,
+};
